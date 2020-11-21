cf12e8bfded522212fa8e4c2fb2f0c2ed261fe26
unknown <#archgowda124@gmail.com>
2020-11-21 18:40:30 +0530

    Added: monthWage

--- EmployeecomputationWage.sh
+++ EmployeecomputationWage.sh
@@ -1,21 +1,27 @@
-#!/bin/bash -x
+#!/bin/bash/
 
-isPartTime=1;
-isFullTime=2;
-empRatePerHrs=20;
-randomcheck=$((RANDOM%3));
-case $empcheck in
-       $isPartTime)
-             empHrs=4;
-             ;;
-       $isFullTime)
-             empHrs=8;
-             ;;
-       *)
-             empHrs=0;
-             ;;
-esac
+IS_PART_TIME=1
+IS_FULL_TIME=2
+EMP_RATE_PER_HRS=20
+NUM_OF_WORKING_DAYS=20
+totalWorkingDays=0
+for ((day=1;day<=NUM_OF_WORKING_DAYS;day++))
+do
+              randomCheck=$(( RANDOM % 3 ))
+                case $randomCheck in
+                        $IS_PART_TIME)
+                                empHrs=4;
+                                ;;
+                        $IS_FULL_TIME)
+                                empHrs=8;
+                                ;;
+                        *)
+                                empHrs=0;
+                                ;;
+           esac
+salary=$((empHrs*$EMP_RATE_PER_HRS))
+totalSalary=$(($totalSalary+$salary))
 
-salary=$((empHrs*$empRatePerHrs));
-echo $salary
+echo "Employee total Salary is : $totalSalary"
+done
 
